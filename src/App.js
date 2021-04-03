956f9f2dd5863a85b70f0a233c46bcbc9f4a8092
Tongleng <leangtongleng6@gmail.com>
2021-04-03 11:59:39 +0700

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello my Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
