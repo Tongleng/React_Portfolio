959486ea0ed7582034b25c3f534271c9ae69a00d
Tongleng <leangtongleng6@gmail.com>
2021-04-03 13:37:02 +0700

    Navbar Completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+
+//Impor React-FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
+
+const Navbar = () => {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="www.google.com">
+            <img className="logo" src={logo} alt="logo..."/>
+            <a>ISecurity</a>
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{color: '#fff'}} />
+          </button>
+ 
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link active" href="">Home<span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="">Protection<span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="">Message<span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="">Contact<span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="">About<span className="sr-only">(current)</span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+}
+export default Navbar;
